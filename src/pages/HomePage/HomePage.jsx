d6adb62ac0a16a3f593edfcf278f8fcd67f73649
Mattias-sleepy <mattias.bjornemark@gmail.com>
2024-07-23 11:44:49 +0200

    Made navbar on HomePage into a component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -25,17 +25,7 @@ export function HomePage() {
                     <img src={WhiteDownArrow} />
                 </div>
                 <nav>
-                    <div className={style.navbar}>
-                        <ul>
-                            <li><Link to="/product">Electronics</Link></li>
-                            <li><a href="URL">Motors</a></li>
-                            <li><a href="URL">Fashion</a></li>
-                            <li><a href="URL">Collectibles & Art</a></li>
-                            <li><a href="URL">Sports</a></li>
-                            <li><a href="URL">Health & Beauty</a></li>
-                            <li><a href="sixthpage.html">More</a></li>
-                        </ul>
-                    </div>
+                    <NavBar />
                 </nav>
             </div>
             <div className={style.green_box}>
---
+++ src/pages/HomePage/NavBar.jsx
@@ -0,0 +1,18 @@
+import style from "./HomePage.module.css"
+export function NavBar() {
+    return (
+        <>
+            <div className={style.navbar}>
+                <ul>
+                    <li><Link to="/product">Electronics</Link></li>
+                    <li><a href="URL">Motors</a></li>
+                    <li><a href="URL">Fashion</a></li>
+                    <li><a href="URL">Collectibles & Art</a></li>
+                    <li><a href="URL">Sports</a></li>
+                    <li><a href="URL">Health & Beauty</a></li>
+                    <li><a href="sixthpage.html">More</a></li>
+                </ul>
+            </div>
+        </>
+    )
+}
